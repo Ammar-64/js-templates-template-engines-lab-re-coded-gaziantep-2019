4911df9a2964e7f8e7f49818e98a242211466278
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-28 22:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-let page-template;
+function createPost(){
+  let title = document.getElementById('postTitle').value;
+  let content = document.getElementById('postContent').value;
+  let auther = document.getElementById('postAuthor').value;
+
+  let postTemplate = document.getElementById('post-template').innerHTML;
+  let postFn = _.template(postTemplate);
+
+  let pageTemplate = document.getElementById('page-template').innerHTML;
+  let pageFn = _.template(pageTemplate)
+
+  let post = postFn({'title': title, 'content': content, 'author': author});
+  let page = pageFn({'post': post})
+
+  let main = document.querySelector('main');
+  main.innerHTML += page;
+
+  let comments = document.querySelector('comments-template');
+  main.innerHTML += comments;
+  
+}
